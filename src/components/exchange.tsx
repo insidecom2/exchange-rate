dd71151a64b2598006ab58110d284e0abc6eeae7
Jakkrirt <Mona Lisa>
2023-01-14 12:25:49 +0700

    feat: update exchange rate fetch

--- src/components/exchange.tsx
+++ src/components/exchange.tsx
@@ -12,68 +12,66 @@ const { Title, Text } = Typography;
 
 export default function Exchange() {
     const [autoFetch, setAutoFetch] = useState<number>(5)
-    const dispatch:Dispatch<any> = useDispatch();
+    const dispatch: Dispatch<any> = useDispatch();
     const param = useTypedSelector((state) => state.param);
     const exchangeRate = useTypedSelector((state) => state.exchangeRate);
     const [data, setData] = useState<typeof exchangeRate>({
         payload: {},
         loading: false,
     });
+    const [pairLasted, setPairLasted] = useState<string>(param.pair);
     const [loadingPage, setLoadingPage] = useState<boolean>(true)
 
     useEffect(() => {
-        if ( Object.keys(exchangeRate.payload).length > 0) {
+        if (Object.keys(exchangeRate.payload).length > 0) {
             setData(exchangeRate)
-            if(loadingPage) setLoadingPage(false)
-        } 
-    },[exchangeRate]);
+            if (loadingPage) setLoadingPage(false)
+        }
+    }, [exchangeRate]);
 
     useEffect(() => {
+        if (pairLasted !== param.pair) {
+            setAutoFetch(5)
+            setPairLasted(param.pair)
+        }
+        const fetchExchangeRate = async () => {
+            if (param.pair) {
+                try {
+                    const symbol = convertSymbolLower(param.pair)
+                    if (!exchangeRate.loading) {
+                        await dispatch(getExchangeRate(symbol));
+                    }
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        }
+
         if (autoFetch === 5) {
             fetchExchangeRate();
         }
         const interval = setInterval(() => {
             if (autoFetch - 1 >= 0) {
-                setAutoFetch(autoFetch - 1);                
+                setAutoFetch(autoFetch - 1);
             } else {
                 setAutoFetch(5)
             }
         }, 1000);
         return () => clearInterval(interval);
-    }, [autoFetch])
+    }, [autoFetch, param.pair])
 
-    useEffect(() => {
-        if (param.pair) {
-            fetchExchangeRate();
-            setAutoFetch(5)
-        }
-    }, [param.pair]);
-    
-    const fetchExchangeRate = async () => {
-        if (param.pair) {
-            try {
-                const symbol = convertSymbolLower(param.pair)
-                if (!exchangeRate.loading) {
-                    await dispatch(getExchangeRate(symbol));
-                }           
-            } catch (error) {
-                console.log(error)
-            }
-        }
-    }
-    
     return (
         <Card style={{ width: '100%', height: '240px' }}>
-            {loadingPage ? 
-            <div className="exchange-loading">
-            <Spin />
-            </div> : ''}
+            {loadingPage ?
+                <div className="exchange-loading">
+                    <Spin />
+                </div> : ''}
             <Title level={3}>{convertSymbolUpper(param.pair)}</Title>
-            <Title level={2} style={{height:'30px'}}>{ data.payload.lastPrice ? moneyFormat(data.payload.lastPrice):'' }</Title>
-            <Text className="exchange-volume-text"><p>Volume : { data.payload.quoteVolume ? moneyFormat(data.payload.quoteVolume):'' }</p></Text>
-            <Text className="exchange-volume-text"><p>High price : { data.payload.highPrice ? moneyFormat(data.payload.highPrice):'' }</p></Text>
+            <Title level={2} style={{ height: '30px' }}>{data.payload.lastPrice ? moneyFormat(data.payload.lastPrice) : ''}</Title>
+            <Text className="exchange-volume-text"><p>Volume : {data.payload.quoteVolume ? moneyFormat(data.payload.quoteVolume) : ''}</p></Text>
+            <Text className="exchange-volume-text"><p>High price : {data.payload.highPrice ? moneyFormat(data.payload.highPrice) : ''}</p></Text>
             <Text className="exchange-volume-text"><p>Low price : {data.payload.lowPrice ? moneyFormat(data.payload.lowPrice) : ''}</p></Text>
-            <Text className={parseFloat(data.payload.priceChangePercent) < 0 ? 'exchange-volume-danger' : 'exchange-volume-success'}><p>Change : { data.payload.priceChangePercent ? moneyFormat(data.payload.priceChangePercent)+'%':'' } </p></Text>
+            <Text className={parseFloat(data.payload.priceChangePercent) < 0 ? 'exchange-volume-danger' : 'exchange-volume-success'}><p>Change : {data.payload.priceChangePercent ? moneyFormat(data.payload.priceChangePercent) + '%' : ''} </p></Text>
         </Card>
     )
 }
