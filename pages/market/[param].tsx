28ee6e3dd52e0e05a707600c11537a216efbb82a
Jakkrirt <Mona Lisa>
2023-01-15 12:19:31 +0700

    feat: responsive on mobile

--- pages/market/[param].tsx
+++ pages/market/[param].tsx
@@ -5,23 +5,21 @@ import Exchange from "../../src/components/exchange";
 import Head from 'next/head'
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-// import { setPair } from "../../src/redux/actions/param.action";
 import { Dispatch } from "redux";
 import { ActionType } from "../../src/redux/actionTypes/param";
-// import { pairSaga, setPairAction } from '../../src/redux/sagas/param.action';
 
 export default function Market() {
     const router = useRouter()
     const { param } = router.query
     const dispatch: Dispatch<any> = useDispatch();
-    
+
     const action = (type, payload) => dispatch({ type, payload })
 
     useEffect(() => {
         if (param) {
-            action(ActionType.GET_PAIR_REQ, param);            
+            action(ActionType.GET_PAIR_REQ, param);
         }
-    },[param])
+    }, [param])
     return (
         <div>
             <Head>
@@ -30,8 +28,8 @@ export default function Market() {
             <Row justify="space-around" align="middle" style={{ 'height': '90vh' }}>
                 <Col span={24}>
                     <Row justify="center" gutter={[16, 16]}>
-                        <Col span={6}><Coin /></Col>
-                        <Col span={6}><Exchange /></Col>
+                        <Col span={6} xs={{ span: 24 }}><Coin /></Col>
+                        <Col span={6} xs={{ span: 24 }}><Exchange /></Col>
                     </Row>
                 </Col>
             </Row>
